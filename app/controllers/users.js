e5108d7b4ed167d6ff43fece35082800a959869c
Gorkem Yurtseven <yurtseven.gorkem@gmail.com>
2013-09-13 18:05:49 -0400

    query

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -19,8 +19,8 @@ exports.login = function(req,res){
 exports.getEmail = function(req, res){
 	console.log(req.body.headers);
 	console.log(req.body.headers.From)
-
-	User.find({email : req.body.headers.From },function(err,user){
+	var senderEmail  = req.body.headers.From.split(">")[0].split("<")[1];
+	User.find({email : senderEmail },function(err,user){
 		if(err)console.log(err);
 		console.log(user);
 		res.writeHead(200, {'content-type': 'text/plain'})
