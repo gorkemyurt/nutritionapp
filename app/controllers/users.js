208b34a7e64bc8da136828482252f77f29e8dd72
Gorkem Yurtseven <yurtseven.gorkem@gmail.com>
2013-09-13 17:39:29 -0400

    200 response

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -18,19 +18,8 @@ exports.login = function(req,res){
 
 exports.getEmail = function(req, res){
 	console.log(req.body.headers);
-	// console.log(req.body.headers.Received);
 	console.log(req.body.headers.From)
+	res.writeHead(200, {'content-type': 'text/plain'})
+	res.end('Message Received. Thanks!\r\n')
 
-	// console.log("from   "  + req.body.headers.envelope.from);
-	// console.log("plain  " + req.body.headers.plain);
-
-
-	// var form = new formidable.IncomingForm()
- //  	form.parse(req, function(err, fields, files) {
-	//     console.log(fields.to)
-	//     console.log(fields.from)
-	//     console.log(fields.subject)
-	//     res.writeHead(200, {'content-type': 'text/plain'})
-	//     res.end('Message Received. Thanks!\r\n')
- //  	})
 }
