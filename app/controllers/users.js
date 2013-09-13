20f89160ebd33b1c759515d2a63837f9f227b36a
Gorkem Yurtseven <yurtseven.gorkem@gmail.com>
2013-09-13 17:34:30 -0400

    conf

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -18,7 +18,8 @@ exports.login = function(req,res){
 
 exports.getEmail = function(req, res){
 	console.log(req.body.headers);
-	console.log(req.body.headers.Received);
+	// console.log(req.body.headers.Received);
+	console.log(req.body.headers.From)
 
 	// console.log("from   "  + req.body.headers.envelope.from);
 	// console.log("plain  " + req.body.headers.plain);
