2536a5bb1b04349691562f909f42b77e8c17dc9c
Gorkem Yurtseven <yurtseven.gorkem@gmail.com>
2013-09-17 18:21:32 -0400

    foundation require.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -15,7 +15,22 @@ require.config({
     "EmailsView" : 'views/EmailsView',
     "EmailView" : 'views/EmailView',
     "NoEmailView" : 'views/NoEmailView',
-    "MyApp" : 'app'
+    "MyApp" : 'app',
+    "foundation": "foundation/foundation",
+    "foundation.alerts": "foundation/foundation.alerts",
+    "foundation.clearing": "foundation/foundation.clearing",
+    "foundation.cookie": "foundation/foundation.cookie",
+    "foundation.dropdown": "foundation/foundation.dropdown",
+    "foundation.forms": "foundation/foundation.forms",
+    "foundation.joyride": "foundation/foundation.joyride",
+    "foundation.magellan": "foundation/foundation.magellan",
+    "foundation.orbit": "foundation/foundation.orbit",
+    "foundation.placeholder": "foundation/foundation.placeholder",
+    "foundation.topbar": "foundation/foundation.topbar",
+    "foundation.reveal": "foundation/foundation.reveal",
+    "foundation.section": "foundation/foundation.section",
+    "foundation.tooltips": "foundation/foundation.tooltips",
+    "foundation.topbar": "foundation/foundation.topbar",
 
   },
   shim: {
@@ -35,20 +50,79 @@ require.config({
     'marionette' : {
       deps : ["backbone", "underscore", "jquery"],
       exports: 'Marionette'
+    },
+    "foundation": {
+            deps: ["jquery"]
+    },
+    "foundation.alerts": {
+        deps: ["foundation"]
+    },
+    "foundation.clearing": {
+        deps: ["foundation"]
+    },
+    "foundation.cookie": {
+        deps: ["foundation"]
+    },
+    "foundation.dropdown": {
+        deps: ["foundation"]
+    },
+    "foundation.forms": {
+        deps: ["foundation"]
+    },
+    "foundation.joyride": {
+        deps: ["foundation"]
+    },
+    "foundation.magellan": {
+        deps: ["foundation"]
+    },
+    "foundation.orbit": {
+        deps: ["foundation"]
+    },
+    "foundation.placeholder": {
+        deps: ["foundation"]
+    },
+    "foundation.reveal": {
+        deps: ["foundation"]
+    },
+    "foundation.section": {
+        deps: ["foundation"]
+    },
+    "foundation.tooltips": {
+        deps: ["foundation"]
+    },
+    "foundation.topbar": {
+        deps: ["foundation"]
     }
-  }
+    },
 });
 
 require(
+
   ["jquery",
     "underscore",
     "backbone",
     "less",
     "marionette",
-    "MyApp"
+    "MyApp",
+    "foundation.alerts",
+    "foundation.clearing",
+    "foundation.cookie",
+    "foundation.dropdown",
+    "foundation.forms",
+    "foundation.joyride",
+    "foundation.magellan",
+    "foundation.orbit",
+    "foundation.placeholder",
+    "foundation.topbar",
+    "foundation.reveal",
+    "foundation.section",
+    "foundation.tooltips",
+    "foundation.topbar",
+
 
   ],
   function($, _, Backbone, jqueryui, less, Marionette, MyApp) {
+    $(document).foundation();
     $('.sendEmail').on('click',function(){
         console.log("why is this not working");
         $.ajax({
