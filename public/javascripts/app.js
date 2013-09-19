35fb7b826b3dee929dd8261c23f2dc049d8e3b3c
Gorkem Yurtseven <yurtseven.gorkem@gmail.com>
2013-09-19 15:28:22 -0400

    welcome page changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -14,11 +14,16 @@ define([
     });
 
     MyApp.addInitializer(function(){
-	      MyApp.emails = new Emails();
-
-	      MyApp.list.show(new EmailsView({collection: MyApp.emails }));
-          MyApp.form.show(new FormView({collection: MyApp.emails}));
-
+          MyApp.emails = new Emails();
+          console.log(MyApp.emails.length);
+         var months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
+          var that = this;
+          MyApp.emails.fetch().complete(function(){       
+                _.each(MyApp.emails.models, function(item){
+                    item.set("Date", months[new Date(item.get("Date")).getMonth()] + " " + new Date(item.get("Date")).getUTCDate());
+                });
+                MyApp.list.show(new EmailsView({collection: MyApp.emails }));
+         });
     });
 
     MyApp.start();
--- public/javascripts/collections/emails.js
+++ public/javascripts/collections/emails.js
@@ -14,13 +14,13 @@ define([
 	    model : emailModel,
 
 	   	initialize : function() {
-	   		var months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
-	   		var that = this;          
-	   		this.fetch().complete(function(){
-		   		_.each(that.models, function(item){
-	                item.set("Date", months[new Date(item.get("Date")).getMonth()] + " " + new Date(item.get("Date")).getUTCDate());
-          		});
-	   		});
+	   		// var months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December'];
+	   		// var that = this;          
+	   		// this.fetch().complete(function(){
+		   	// 	_.each(that.models, function(item){
+	     //            item.set("Date", months[new Date(item.get("Date")).getMonth()] + " " + new Date(item.get("Date")).getUTCDate());
+      //     		});
+	   		// });
 
 	   	}
 	
